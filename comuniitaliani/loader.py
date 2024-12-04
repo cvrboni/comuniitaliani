96a364b2266a67d52c469f707c44c45270cc16ae
cvrboni <andreacarboni@stepservizi.net>
2024-12-04 12:28:34 +0100

    pulizia restituzione dati

--- comuniitaliani/loader.py
+++ comuniitaliani/loader.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 ISTAT_CSV_URL = "http://www.istat.it/storage/codici-unita-amministrative/Elenco-comuni-italiani.csv"
 
+
 def get_remote_last_modified():
     """Recupera la data dell'ultima modifica del CSV remoto."""
     response = requests.head(ISTAT_CSV_URL)
@@ -14,11 +15,13 @@ def get_remote_last_modified():
             return datetime.strptime(remote_last_modified, '%a, %d %b %Y %H:%M:%S %Z')
     raise Exception("Impossibile ottenere la data di ultima modifica del file remoto.")
 
+
 def get_local_last_modified(local_path):
     """Recupera la data di ultima modifica del file locale."""
     if os.path.exists(local_path):
         return datetime.fromtimestamp(os.path.getmtime(local_path))
     return None
+
 
 def is_csv_updated(local_path="database/comuni.csv"):
     """Controlla se il file CSV locale è aggiornato rispetto alla versione remota."""
@@ -32,6 +35,7 @@ def is_csv_updated(local_path="database/comuni.csv"):
         print(f"Errore durante il controllo dell'aggiornamento: {e}")
         return False
 
+
 def download_csv(local_path="database/comuni.csv"):
     """Scarica il file CSV dell'ISTAT e lo salva localmente."""
     response = requests.get(ISTAT_CSV_URL)
@@ -43,6 +47,12 @@ def download_csv(local_path="database/comuni.csv"):
     else:
         raise Exception("Errore durante il download del file CSV.")
 
+
+def clean_column_names(columns):
+    """Pulisce e rinomina le colonne per semplificarne l'uso."""
+    return columns.str.strip().str.replace('\n', ' ').str.replace('  ', ' ').str.lower()
+
+
 def load_comuni_data(csv_file="database/comuni.csv"):
     """Carica i dati dei comuni dal file CSV."""
     if not is_csv_updated(csv_file):
@@ -50,4 +60,20 @@ def load_comuni_data(csv_file="database/comuni.csv"):
         download_csv(csv_file)
     else:
         print("Il file CSV è già aggiornato.")
-    return pd.read_csv(csv_file, encoding='latin1', delimiter=';')
+
+    # Carica il CSV
+    data = pd.read_csv(csv_file, encoding='latin1', delimiter=';')
+
+    # Pulisce i nomi delle colonne
+    data.columns = clean_column_names(data.columns)
+
+    # Rinomina le colonne per semplicità
+    data.rename(columns={
+        "denominazione in italiano": "denominazione_italiana",
+        "sigla automobilistica": "sigla_provincia",
+        "denominazione regione": "regione",
+        "denominazione dell'unità territoriale sovracomunale (valida a fini statistici)": "provincia",
+        "codice catastale del comune": "codice_catastale"
+    }, inplace=True)
+
+    return data
